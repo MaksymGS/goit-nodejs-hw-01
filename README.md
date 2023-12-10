ee03c17f1eebe197686cd5188869a023db232520
MaksymGS <130307586+MaksymGS@users.noreply.github.com>
2023-12-11 00:47:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+node index.js --action="list"
+https://monosnap.com/file/I3KyoKPz7RR0nf6ktR4DWqqyReGQvd
+
+# Отримуємо контакт по id і виводимо у консоль об'єкт контакту або null, якщо контакту з таким id не існує.
+node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6
+https://monosnap.com/file/C0Ac03mdCdPayfXWVtpssG1Z4wPMB5
+
+# Додаємо контакт та виводимо в консоль об'єкт новоствореного контакту
+node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22
+https://monosnap.com/file/JSmg8VJwPn1eYtwwisKEHrn2Nd3vTZ
+
+# Видаляємо контакт та виводимо в консоль об'єкт видаленого контакту або null, якщо контакту з таким id не існує.
+node index.js --action="remove" --id qdggE76Jtbfd9eWJHrssH
+https://monosnap.com/file/M4bqflacARx8kT7MOM8ZfxkScuQHzH
---
+++ contacts.js
@@ -0,0 +1,38 @@
+import fs from "fs/promises";
+import { nanoid } from "nanoid";
+import path from "path";
+
+const contactsPath = path.resolve("db", "contacts.json");
+
+const updateContacts = (contactList) =>
+  fs.writeFile(contactsPath, JSON.stringify(contactList, null, 2));
+
+export const listContacts = async () => {
+  const contactsList = await fs.readFile(contactsPath);
+  return JSON.parse(contactsList);
+};
+
+export const getContactById = async (contactId) => {
+  const contactsList = await listContacts();
+  const contact = contactsList.find((item) => item.id === contactId);
+  return contact || null;
+};
+
+export const removeContact = async (contactId) => {
+  const contactsList = await listContacts();
+  const idx = contactsList.findIndex((item) => item.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const [removedContact] = contactsList.splice(idx, 1);
+  await updateContacts(contactsList);
+  return removedContact;
+};
+
+export const addContact = async (name, email, phone) => {
+  const contactsList = await listContacts();
+  const newContact = { name, email, phone, id: nanoid() };
+  contactsList.push(newContact);
+  await updateContacts(contactsList);
+  return newContact;
+};
